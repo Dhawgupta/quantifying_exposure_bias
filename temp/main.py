bdbda2bee7b921e2b6da47ac953ee76034373f4e
jfalus <jfalus@umass.edu>
2023-05-05 21:37:35 +0000

    Add logging to main

--- temp/main.py
+++ temp/main.py
@@ -1,5 +1,6 @@
 from model import get_gpt2, run_model_batch
 from get_data import get_wikitext_2
+from logger import logger
 
 dataset = get_wikitext_2()
 model, tokenizer = get_gpt2()
@@ -12,11 +13,14 @@ import pandas as pd
 
 df = pd.DataFrame([], columns=['input','output'])
 
-for batch in batches:
+for i,batch in enumerate(batches):
+    logger.info(f"Running batch {i+1}/{len(batches)}")
     res = run_model_batch(model, tokenizer, batch)
     res_trimmed = [res.split('\n')[0] for res in res]
     res_pairs = [[batch[i], res_trimmed[i]] for i in range(len(batch))]
     batch_df = pd.DataFrame(res_pairs, columns=['input','output'])
+    logger.info(f"Results from batch {i+1}/{len(batches)}:")
+    logger.info(batch_df)
     df = pd.concat([df, batch_df], ignore_index=True)
     print(df)
 
